e3d0562b7664d839007b1915237919f14d71cb8b
Juan Medina <juanmedinatm@unicauca.edu.co>
2026-03-18 00:11:34 -0500

    SPA casi completa, falta por completar el formulario de contacto

---
+++ SPA/startbootstrap-creative-gh-pages/js/scripts.js
@@ -0,0 +1,77 @@
+/*!
+* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+    gsap.to(document.getElementById("myName"), {
+        duration: 2, 
+        scrambleText: {
+        text: "Juan Pablo Medina Bolanios", 
+        chars: "010", 
+        revealDelay: 1, 
+        speed: 0.2 
+        },
+        onComplete: () => {
+        // Esta arranca cuando termina el scramble
+        gsap.to("#myName", {
+            duration: 2,
+            opacity: 0.7,
+            yoyo: true,        // regresa al estado original
+            repeat: -1         // -1 = infinito
+        });
+    }
+    });
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
